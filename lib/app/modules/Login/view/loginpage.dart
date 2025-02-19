19eedbd50a547665c2c73ddcc9a8d0f1a87d88fa
ganidande905 <ganidande9@gmail.com>
2025-02-20 01:45:38 +0530

    Changes Made : Added a Footer

--- lib/app/modules/Login/view/loginpage.dart
+++ lib/app/modules/Login/view/loginpage.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:v_track_it/app/modules/Login/controller/login_controller.dart';
 
-
+import 'package:footer/footer.dart';
+import 'package:footer/footer_view.dart';
 
 class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
@@ -16,33 +19,55 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(title: Text("Login with OTP")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: usernameController,
-              decoration: InputDecoration(labelText: "Username"),
-            ),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: "Email"),
-            ),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(labelText: "Password"),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () => loginController.registerUser(
-                  context, usernameController, emailController, passwordController),
-              child: Text("Register"),
-            ),
-          ],
+      appBar: AppBar(title: Text("VTrackIt")),
+      body: FooterView(
+        footer: Footer(
+          backgroundColor: Colors.grey[200],
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              
+              Text(
+                "💡 Developed by amF<>SS ",
+                style: TextStyle(fontSize: 14),
+              ),
+
+              
+            ],
+          ),
         ),
+        children: [
+          Padding(
+            padding: EdgeInsets.all(height * 0.03),
+            child: Column(
+              children: [
+                TextField(
+                  controller: usernameController,
+                  decoration: InputDecoration(labelText: "Username"),
+                ),
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(labelText: "Email"),
+                ),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: "Password"),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => loginController.registerUser(
+                      context, usernameController, emailController, passwordController),
+                  child: Text("Register"),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
