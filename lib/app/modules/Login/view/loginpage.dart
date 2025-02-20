460902d1863406acf5c4e3a5b0b15d350278b5a1
ganidande905 <ganidande9@gmail.com>
2025-02-20 20:17:04 +0530

    Changes Made : Removed the appbar and also the footer, changed few styling

--- lib/app/modules/Login/view/loginpage.dart
+++ lib/app/modules/Login/view/loginpage.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:v_track_it/app/modules/Login/controller/login_controller.dart';
-
-import 'package:footer/footer.dart';
-import 'package:footer/footer_view.dart';
+import 'package:v_track_it/app/utils/constants.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -19,55 +17,44 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: AppBar(title: Text("VTrackIt")),
-      body: FooterView(
-        footer: Footer(
-          backgroundColor: Colors.grey[200],
-          padding: EdgeInsets.all(10.0),
+      backgroundColor: kSecondaryBackgroundColor,
+      body: Center(
+
+        child: Padding(
+          padding: EdgeInsets.all(height * 0.03),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               
-              Text(
-                "💡 Developed by amF<>SS ",
-                style: TextStyle(fontSize: 14),
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(labelText: "Username"),
+                style: TextStyle(color: kPrimaryTextColor),
               ),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(labelText: "Email",),
+                style: TextStyle(color: kPrimaryTextColor),
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(labelText: "Password"),
+                style: TextStyle(color: kPrimaryTextColor),               
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => loginController.registerUser(
+                    context, usernameController, emailController, passwordController),
+                child: Text("Register"),
 
-              
+              ),
             ],
           ),
         ),
-        children: [
-          Padding(
-            padding: EdgeInsets.all(height * 0.03),
-            child: Column(
-              children: [
-                TextField(
-                  controller: usernameController,
-                  decoration: InputDecoration(labelText: "Username"),
-                ),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(labelText: "Email"),
-                ),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(labelText: "Password"),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () => loginController.registerUser(
-                      context, usernameController, emailController, passwordController),
-                  child: Text("Register"),
-                ),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
