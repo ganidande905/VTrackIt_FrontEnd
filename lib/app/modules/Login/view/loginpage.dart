d0c8548bd70d8e2d7d56de9ca2bc783fb9fd951c
ganidande905 <ganidande9@gmail.com>
2025-02-25 22:27:59 +0530

    Changed Made : Developed Login Page(Register Page)

--- lib/app/modules/Login/view/loginpage.dart
+++ lib/app/modules/Login/view/loginpage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:v_track_it/app/modules/Login/controller/login_controller.dart';
 import 'package:v_track_it/app/utils/constants.dart';
+import 'package:v_track_it/app/utils/customdropdown.dart';
+import 'package:v_track_it/app/utils/customtextfield.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,49 +12,61 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController usernameController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final LoginController loginController = LoginController();
-
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _mailController = TextEditingController();
+  final List<String> hostelNames = ['Hostel A', 'Hostel B', 'Hostel C'];
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
-
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: kSecondaryBackgroundColor,
-      body: Center(
-
+      backgroundColor: kPrimaryBackgroundColor,
+      body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(height * 0.03),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              
-              TextField(
-                controller: usernameController,
-                decoration: InputDecoration(labelText: "Username"),
-                style: TextStyle(color: kPrimaryTextColor),
-              ),
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(labelText: "Email",),
-                style: TextStyle(color: kPrimaryTextColor),
-              ),
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(labelText: "Password"),
-                style: TextStyle(color: kPrimaryTextColor),               
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () => loginController.registerUser(
-                    context, usernameController, emailController, passwordController),
-                child: Text("Register"),
-
-              ),
-            ],
+          padding: EdgeInsets.only(top: 0, left: width * 0.08, right: width*0.08),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: height*0.02, bottom : height*0.02),
+                  child: Image.asset(
+                      'lib/app/utils/icons/Amrita_Vishwa_Vidyapeetham_-_Logo_Icon.svg.png',
+                      ),
+                ),
+                CustomTextField(
+                  label: 'MAIL',
+                  controller: _mailController,
+                ),
+                CustomTextField(
+                  label: 'USERNAME',
+                  controller: _usernameController,
+                ),
+                CustomDropdown(
+                  hostelNames: hostelNames,
+                ),
+                Padding(
+                  padding:  EdgeInsets.all(height*0.05),
+                  child: SizedBox(
+                    width: width*0.9,
+                    child: ElevatedButton(onPressed: (){},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: kUnderLineEnabled,
+                      
+                    ),
+                    child: const Text('REGISTER',
+                    style: TextStyle(color: kPrimaryBackgroundColor),),),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(height*0.09),
+                  child: SizedBox(height: height*0.1,
+                  child: 
+                  Text('Developed by amF<>SS'),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 
 // Light Theme Color Palette
 const Color kPrimaryBackgroundColor = Color(0xffFFFFFF); // Background: White
-const Color kSecondaryBackgroundColor = Color.fromARGB(255, 0, 0, 0); // Secondary Background
-const Color kPrimaryTextColor = Color.fromARGB(255, 255, 254, 254); // Text: white
-const Color kSecondaryTextColor = Color.fromARGB(255, 0, 0, 0); // Secondary Text: black
+const Color kSecondaryBackgroundColor =
+    Color.fromARGB(255, 0, 0, 0); // Secondary Background
+const Color kPrimaryTextColor =
+    Color.fromARGB(255, 255, 254, 254); // Text: white
+const Color kSecondaryTextColor =
+    Color.fromARGB(255, 0, 0, 0); // Secondary Text: black
 const Color kButtonColor = Color(0xff000000); // Buttons: Black
 const Color kButtonTextColor = Color(0xffFFFFFF); // Button Text: White
 const Color kNavbarColor = Color(0xffFFFFFF); // Navbar Background: White
 const Color kNavbarIconColor = Color(0xff000000); // Navbar Icons: Black
 const Color kBorderColor = Color(0xffE0E0E0); // Input Borders
-
+const Color kUnderLineEnabled = Color(0xFFBA294B);
 // Font Configuration
 const String kFontFamily = 'Poppins';
 
